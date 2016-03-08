244af37bad45f0de642a2cbf8e694763a396b4a4
Sumayyah Perine <sperine01@gmail.com>
2016-03-08 01:11:03 -0500

    updated portfolio section

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,11 @@
-var hoverItem = document.querySelector('.wrapper');
+// var hoverItem = document.querySelector('.wrapper');
 
-hoverItem.addEventListener('click', function(e) {
-  e.preventDefault();
-}, false);
+// hoverItem.addEventListener('click', function(e) {
+//   e.preventDefault();
+// }, false);
 
-hoverItem.addEventListener('mousemove', function(evt) {
-  evt = (evt || event);
+// hoverItem.addEventListener('mousemove', function(evt) {
+//   evt = (evt || event);
   
-  hoverItem.style.backgroundPosition = (evt.clientX/20) + '%' + ' ' + (evt.clientY/20) + '%';
-}, false);
+//   hoverItem.style.backgroundPosition = (evt.clientX/20) + '%' + ' ' + (evt.clientY/20) + '%';
+// }, false);
